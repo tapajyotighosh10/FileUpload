ac4327a9b8830012781e5ec5d891a3ea61a7725d
Tapajyoti Ghosh <tapajyotighosh10@gmail.com>
2023-11-08 13:33:52 +0530

    Change in only pdf file upto 2mb is allowes and details

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
+
+	//"math"
 	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 
 	// "strconv"
 	// "strings"
@@ -24,12 +29,19 @@ func main() {
 	}
 }
 
+type FileDetails struct {
+	FileName string `json:"fileName"`
+	FileSize float64 `json:"fileSize"`
+}
+
 func handleUpload(c *gin.Context) {
 	form, err := c.MultipartForm()
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
+	var uploadedFiles []FileDetails
 
 	files := form.File["files"]
 	for key, file := range files {
@@ -38,17 +50,29 @@ func handleUpload(c *gin.Context) {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
+		// Calculate file size in MB
+		fileSize := float64(file.Size)/(1024*1024)
+
+		// Collect file details
+		uploadedFiles = append(uploadedFiles, FileDetails{
+			FileName: file.Filename,
+			FileSize: fileSize,
+		})
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Files uploaded successfully"})
+	c.JSON(http.StatusOK, gin.H{"message": "Files uploaded successfully", "files": uploadedFiles})
 }
 
 func saveFile(fileHeader *multipart.FileHeader, key int) error {
 	fmt.Println(fileHeader.Filename)
-	// ext := strings.Split(fileHeader.Filename, ".")
-	// if ext[1] != "pdf" {
-	// 	return errors.New("pass a valid pdf")
-	// }
+	ext := strings.Split(fileHeader.Filename, ".")
+	if len(ext) < 2 || ext[1] != "pdf" {
+		return errors.New("pass a valid pdf")
+	}
+
+	if fileHeader.Size > 2*1024*1024 {
+		return errors.New("File size exceeds the 2MB limit")
+	}
 
 	src, err := fileHeader.Open()
 	if err != nil {
@@ -58,8 +82,8 @@ func saveFile(fileHeader *multipart.FileHeader, key int) error {
 	defer src.Close()
 
 	// Create a new file in the desired destination folder
-	dstPath := filepath.Join("./Storage", fileHeader.Filename)   //ext[0]+strconv.Itoa(key)+"."+ext[1]
-	
+	dstPath := filepath.Join("./Storage", ext[0]+strconv.Itoa(key)+"."+ext[1]) // //fileHeader.Filename
+
 	dst, err := os.Create(dstPath)
 	if err != nil {
 		return err
